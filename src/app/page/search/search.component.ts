a3e6822fb5cf80ff1381f632caf9f2291a131a0a
analayze <annalisa@antonioli.dev>
2025-06-17 19:11:03 +0200

    Fix search compo

--- src/app/page/search/search.component.ts
+++ src/app/page/search/search.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+
+import { Movie } from '../../models/movie.model';
+import { MovieService } from '../../services/movie.service';
+import { Genres, Genre } from '../../models/utility.models';
+import { FormUtilsService } from '../../services/formUtils.service';
+
+import { MovieListComponent } from '../../components/movie-list/movie-list.component';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MovieListComponent],
   templateUrl: './search.component.html',
-  styleUrl: './search.component.scss'
+  styleUrl: './search.component.scss',
 })
-export class SearchComponent {
-
+export class SearchComponent implements OnInit {
+  movies: Movie[] = [];
+  genres: Genre[] = [];
+  years: string[] = [];
+  // private movieService = inject(MovieService);
+  private formUtilsService = inject(FormUtilsService);
+  ngOnInit(): void {
+    const currentYear = new Date().getFullYear();
+    for (let year = currentYear; year >= 1900; year--) {
+      this.years.push(year.toString());
+    }
+    console.log(this.years);
+    this.formUtilsService.getGenres().subscribe({
+      next: (genres) => {
+        if (!genres) return;
+        this.genres = genres.genres;
+      },
+      error: (err) => console.log(err),
+    });
+  }
 }
