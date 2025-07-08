79f22a4d2237560cfd49aeda72be2d1f530f3cc7
analayze <annalisa@antonioli.dev>
2025-07-08 10:20:42 +0200

    log for test

--- replace-env.js
+++ replace-env.js
@@ -2,15 +2,26 @@
 const fs = require("fs");
 const path = require("path");
 
-// Leggi il file environment.prod.ts
+// Percorso del file environment.prod.ts
 const envProdFilePath = path.resolve(__dirname, "src/envs/env.prod.ts");
 
 // Leggi il contenuto del file environment.prod.ts
 let content = fs.readFileSync(envProdFilePath, "utf-8");
+console.log("Starting replace-env.js...");
 
-// Sostituisci il token con la variabile di ambiente fornita da Vercel
-const apiToken = process.env.NG_APP_BEARER_TOKEN || ""; // Puoi sostituire con un valore di default
-content = content.replace("token", `'${apiToken}'`);
+// Ottieni il valore della variabile di ambiente NG_APP_BEARER_TOKEN (da Vercel o locale)
+const apiToken = process.env.NG_APP_BEARER_TOKEN || ""; // Usa un valore di default se non è presente
+
+// Debug: Stampa il valore della variabile di ambiente per assicurarti che sia corretto
+console.log("API Token:", apiToken);
+
+// Sostituisci la stringa 'process.env.NG_APP_BEARER_TOKEN' con il valore della variabile di ambiente
+content = content.replace(
+  /'process.env.NG_APP_BEARER_TOKEN'/g, // Cerca la stringa con le virgolette
+  `'${apiToken}'` // Sostituisci con il valore della variabile d'ambiente
+);
 
 // Scrivi il contenuto modificato nel file
 fs.writeFileSync(envProdFilePath, content);
+
+console.log("File environment.prod.ts aggiornato con il token API.");
